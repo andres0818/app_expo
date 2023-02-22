76350a21a72d063198db2e6d057bb221f22b5a98
andres0818 <c.andres.perez.h@gmail.com>
2023-02-22 12:58:00 -0500

    add component repositoriesList

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Constants from "expo-constants";
+import { RepositoryList } from "./data/RepositoryList";
+
+export const Main = () => {
+  return (
+    <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
+      <RepositoryList />
+    </View>
+  );
+};
