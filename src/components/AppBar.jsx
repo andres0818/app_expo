bf9155e49fd1560f1bda9af23d41c508e6e49eca
andres0818 <c.andres.perez.h@gmail.com>
2023-02-23 00:57:18 -0500

    create scroll and location observer

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import StyledText from "./StyledText";
 import Constanst from "expo-constants";
 import theme from "../thme";
-import { Link } from "react-router-native";
+import { Link, useLocation } from "react-router-native";
 
-const AppBarTab = ({ active, children, to }) => {
+const AppBarTab = ({ children, to }) => {
+  const { pathname } = useLocation();
+  const active = pathname === to;
+  const textStyles = [styles.text, active && styles.active];
+
   return (
     <Link to={to}>
-      <StyledText fontWeight="bold" style={styles.text}>
+      <StyledText fontWeight="bold" style={textStyles}>
         {children}
       </StyledText>
     </Link>
@@ -18,21 +22,31 @@ const AppBarTab = ({ active, children, to }) => {
 export const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab active to='/'>Repositories</AppBarTab>
-      <AppBarTab active to='/singin'>Singin</AppBarTab>
+      <ScrollView horizontal style={styles.scroll}>
+        <AppBarTab active to="/">
+          Repositories
+        </AppBarTab>
+        <AppBarTab to="/signin">Sign In</AppBarTab>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection:"row",
+    flexDirection: "row",
     backgroundColor: theme.appBar.primary,
     paddingTop: Constanst.statusBarHeight + 10,
-    paddingBottom: 10,
+  },
+  scroll: {
+    paddingBottom: 15,
+    marginHorizontal: 10,
   },
   text: {
+    color: theme.appBar.textSecondary,
+    paddingLeft: 10,
+  },
+  active: {
     color: theme.appBar.textPrimary,
-    paddingLeft:10
   },
 });
