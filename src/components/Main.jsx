7e63c475273603cd92f1f29f152fd6d37e959a39
andres0818 <c.andres.perez.h@gmail.com>
2023-02-23 00:56:12 -0500

    spelling correction

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,7 @@ export const Main = () => {
       <AppBar />
       <Routes>
         <Route exact path="/" element={<RepositoryList />} />
-        <Route exact path="/singin" element={<Text>Working in it</Text>} />
+        <Route exact path="/signin" element={<Text>Working in it</Text>} />
       </Routes>
     </View>
   );
