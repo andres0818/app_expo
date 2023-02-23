8497e2a7166d0f2ef3b2fbef8a07e6d045af7d15
andres0818 <c.andres.perez.h@gmail.com>
2023-02-23 00:29:46 -0500

    create link home and singin

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,25 +3,36 @@ import { StyleSheet, View } from "react-native";
 import StyledText from "./StyledText";
 import Constanst from "expo-constants";
 import theme from "../thme";
+import { Link } from "react-router-native";
+
+const AppBarTab = ({ active, children, to }) => {
+  return (
+    <Link to={to}>
+      <StyledText fontWeight="bold" style={styles.text}>
+        {children}
+      </StyledText>
+    </Link>
+  );
+};
 
 export const AppBar = () => {
   return (
     <View style={styles.container}>
-      <StyledText fontWeight="bold" style={styles.text}>
-        Repositories
-      </StyledText>
+      <AppBarTab active to='/'>Repositories</AppBarTab>
+      <AppBarTab active to='/singin'>Singin</AppBarTab>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection:"row",
     backgroundColor: theme.appBar.primary,
     paddingTop: Constanst.statusBarHeight + 10,
     paddingBottom: 10,
-    paddingLeft: 10,
   },
   text: {
     color: theme.appBar.textPrimary,
+    paddingLeft:10
   },
 });
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Route, Router, Routes } from "react-router-native";
+
 import { RepositoryList } from "./RepositoryList";
 import { AppBar } from "./AppBar";
 
 export const Main = () => {
   return (
     <View style={{ flex: 1 }}>
-      <AppBar/>
-      <RepositoryList />
+      <AppBar />
+      <Routes>
+        <Route exact path="/" element={<RepositoryList />} />
+        <Route exact path="/singin" element={<Text>Working in it</Text>} />
+      </Routes>
     </View>
   );
 };
